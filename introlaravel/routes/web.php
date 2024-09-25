5517c10baa7d2e0f196de98114eaf1e070ef8505
Antoniogtzv <125924468+Antoniogtzv@users.noreply.github.com>
2024-09-24 19:01:15 -0600

    Avance practica3

---
+++ introlaravel/routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+//RUTA TIPO GET
+/* Route::get('/', function () {
+    return view('welcome');
+}); */
+
+// RUTA TIPO VIEW
+Route::view('/','welcome');
+
+
+//CREACION DE RURTA DE NUESTRO FORMULARIO DE VISTA 
+//RUTA TIPO VIEW
+Route::view('/form','formulario');
